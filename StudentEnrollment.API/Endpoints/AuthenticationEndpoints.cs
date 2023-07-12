e581a886ab6689cc01e530571824e1277ccdd13e
Stephen Hollingsworth <633706+Merganser2@users.noreply.github.com>
2023-07-11 19:06:58 -0700

    Add Registration Endpoint

--- StudentEnrollment.API/Endpoints/AuthenticationEndpoints.cs
+++ StudentEnrollment.API/Endpoints/AuthenticationEndpoints.cs
@@ -5,6 +5,7 @@ using StudentEnrollment.Data;
 using Microsoft.AspNetCore.Identity;
 using StudentEnrollment.API.DTOs.Authentication;
 using StudentEnrollment.Api.Services;
+using StudentEnrollment.API.DTOs;
 
 namespace StudentEnrollment.API.Endpoints
 {
@@ -25,6 +26,33 @@ namespace StudentEnrollment.API.Endpoints
             .WithName("Login")
             .Produces(StatusCodes.Status200OK)
             .Produces(StatusCodes.Status401Unauthorized);
+
+            routes.MapPost("/api/register", async (RegisterDto registerDto, IAuthManager authManager) =>
+            {
+                var response = await authManager.Register(registerDto);
+
+                if (!response.Any())
+                {
+                    return Results.Ok();
+                }
+
+                var errors = new List<ErrorResponseDto>();
+                foreach (var error in response)
+                {
+                    errors.Add(new ErrorResponseDto
+                    {
+                        Code = error.Code,
+                        Description = error.Description
+                    });
+                }
+
+                return Results.BadRequest(errors);
+            })
+            .WithTags("Authentication")
+            .WithName("Register")
+            .Produces(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status400BadRequest);
+
         }
     }
 }
--- StudentEnrollment.API/Services/IAuthManager.cs
+++ StudentEnrollment.API/Services/IAuthManager.cs
@@ -1,10 +1,12 @@
-﻿using StudentEnrollment.API.DTOs.Authentication;
+﻿using Microsoft.AspNetCore.Identity;
+using StudentEnrollment.API.DTOs.Authentication;
 
 namespace StudentEnrollment.Api.Services
 {
     public interface IAuthManager
     {
         Task<AuthResponseDto> Login(LoginDto loginDto);
+        Task<IEnumerable<IdentityError>> Register(RegisterDto registerDto);
     }
 
 }
